09958a2a4b6d2ecddee76e1585e115e54562dc30
somanath <somanath.mishra@planetsbrain.com>
2017-09-20 12:49:32 +0530

    Added for pull request

--- ConsoleGit/Program.cs
+++ ConsoleGit/Program.cs
@@ -8,16 +8,11 @@ namespace ConsoleGit
 {
     class Program
     {
-        //pull develoment and featureIssue2 code
-        
-        private string name="Git_Flow-1";
-       public static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            //new code to checkin
-            Console.WriteLine("Git_Flow-2");
-            Console.WriteLine("Git_Flow-1");
-            Console.WriteLine("Git-flow-test");
+            Console.WriteLine("Pull test");
         }
-        
+
     }
 }
